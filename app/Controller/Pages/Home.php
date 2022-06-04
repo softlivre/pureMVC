28706261714af76848410a1515118842e2521ba8
softlivre <softlivre1@gmail.com>
2022-06-04 15:12:05 -0300

    finished my pureMVC structure with a model

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller\Pages;
 use \App\Utils\View;
+use \App\Model\Entity\Organization;
 
 class Home extends Page{
    public static function getHome(){
-       $content = View::render('pages/home',[
-           'name' => 'Home',
-           'description' => 'Home page',
-           'site' => 'http://www.google.com'
-       ]);
 
-       return parent::getPage('Home 111', $content);
+    $obOrganization = new Organization;    
 
+    $content = View::render('pages/home',[
+        'name' => $obOrganization->name,
+        'description' => $obOrganization->description,
+        'site' => $obOrganization->site
+    ]);
+
+    return parent::getPage('Home 111', $content);
    } 
 }
---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization
+{
+    public $id;
+    public $name = "organization name";
+    public $site = "https://softlivre.com.br";
+    public $description = "organization description";
+
+}
