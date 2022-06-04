0b6a962d3abbfb1e3e08ba803b66f21cc513cb38
softlivre <softlivre1@gmail.com>
2022-06-04 14:56:35 -0300

    finished basic elements of a page, before starting to use a model

--- app/Controller/Pages/Page.php
+++ app/Controller/Pages/Page.php
@@ -3,17 +3,23 @@
 namespace App\Controller\Pages;
 use \App\Utils\View;
 
-class Page{
+class Page
+{
 
-private static function getHeader(){
-    return View::render('pages/header');
-}
+    private static function getHeader(){
+        return View::render('pages/header');
+    }
 
-   public static function getPage($title, $content){
-       return View::render('pages/page',[
-           'title' => $title,
-           'header' => self::getHeader(),
-           'content' => $content     
-       ]);
-   } 
+    private static function getFooter(){
+        return View::render('pages/footer');
+    }
+
+    public static function getPage($title, $content){
+        return View::render('pages/page',[
+            'title' => $title,
+            'header' => self::getHeader(),
+            'content' => $content,
+            'footer' => self::getFooter(),    
+        ]);
+    } 
 }
