bcaa543f89b71ebf7283ef574cb7ece5e2f37b67
softlivre <softlivre1@gmail.com>
2022-10-28 19:26:02 -0300

    preparing to use .env

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// read .env variables
+
+
 require __DIR__.'/vendor/autoload.php';
 
 use \App\Controller\Pages\Home;
 
 echo Home::getHome();
+
+
+
+
